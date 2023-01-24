6d78bb5825c88a55c1b76812eb823a9cc278c67c
Vishal9236 <123462520+Vishal9236@users.noreply.github.com>
2023-01-24 16:18:01 +0530

    Delete delimiter.js

--- delimiter.js
+++
@@ -1,7 +0,0 @@
-let input = prompt("Enter a name or sentence :").split("");
-
-let output = input.reduce((a,b) => `${a}${b}.`, "").toUpperCase();
-alert(output);
-
-let enterAgain = confirm("Want to Enter Again?");
-enterAgain ? location.reload() : alert("Thanks, see you again.");
